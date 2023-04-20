33d36bf817fa40cc883b15bc93b006b177c4fc19
Amoniack <ramatavio25@gmail.com>
2023-04-19 23:38:57 -0300

    segunda entrega

--- javascript/script.js
+++ javascript/script.js
@@ -52,10 +52,33 @@ const comprador = () => {
        ) if (consultaInstrumento == "bajo "|| consultaInstrumento == "Bajo");
     } while (consultaInstrumento == "" || consultaInstrumento == null); console.log("el cliente se intereso por " + consultaInstrumento)
     return consultaInstrumento
+}   
+const catalogo = {
+    guitarras: [
+      { nombre: "Guitarra eléctrica Fender Stratocaster", precio: 15000, stock: 5 },
+      { nombre: "Guitarra acústica Yamaha FG800", precio: 8000, stock: 10 },
+      { nombre: "Guitarra clásica Admira A5", precio: 6000, stock: 3 },
+    ],
+    bajos: [
+      { nombre: "Bajo eléctrico Fender Precision Bass", precio: 17000, stock: 3 },
+      { nombre: "Bajo acústico Ibanez PCBE12MH", precio: 12000, stock: 8 },
+      { nombre: "Bajo de 6 cuerdas Warwick Corvette", precio: 23000, stock: 1 },
+    ],
+    baterias: [
+      { nombre: "Batería electrónica Roland TD-17KV", precio: 45000, stock: 2 },
+      { nombre: "Batería acústica Gretsch Catalina Maple", precio: 35000, stock: 4 },
+      { nombre: "Batería infantil RockJam RJ101-MB", precio: 2000, stock: 6 },
+    ],
+  };
+for (let i = 0; i < catalogo.length; index++) {
+    console.log(catalogo[i].nombre)
+    console.log(catalogo[i].precio)
+    console.log(catalogo[i].stock)
 }
     
 Bienvenida()
 Consulta()
 instrumento()
+catalogo()
 }
 comprador()
